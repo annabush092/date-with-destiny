04778f1b1ec4af297890a2b950f06dac4cfad448
annabush092 <annabush092@gmail.com>
2017-10-06 09:41:58 -0400

    fixed address validation bug

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -4,13 +4,15 @@ class Plan < ApplicationRecord
   belongs_to :nightlife
   belongs_to :activity
 
+  validates :date, presence: true
   validate :date_checker
 
   def date_checker
-    if self.date < Date.today
-      self.errors[:date] << "Date cannot be in the past"
+    if self.date
+      if self.date < Date.today
+        self.errors[:date] << "Date cannot be in the past"
+      end
     end
   end
-
 
 end
